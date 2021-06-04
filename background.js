7bee0c90d46d660a5c4097b9bf030b48a8990eb8
Victor Kim <34536619+WojtekTB@users.noreply.github.com>
2021-06-03 21:46:02 -0700

    adding a donation.html

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.executeScript(null, {file: "importGameToLichess.js"});
-});
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == "complete") {
         chrome.tabs.executeScript(null, {file: "injectHtmlButtonToPage.js"});
